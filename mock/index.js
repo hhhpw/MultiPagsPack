628c22cbe1683f5fe0704619b48ea4f3734c94fc
haopeiwei <haopeiwei@zhidaoauto.com>
2018-11-30 15:28:26 +0800

    '增加mock数据'

---
+++ mock/index.js
@@ -0,0 +1,42 @@
+// import Mock from 'mockjs';
+const Mock = require('mockjs');
+const Random = Mock.Random;
+
+Mock.mock('/mock/test', 'get', function(opts) {
+    return opts.type;
+})
+
+Mock.mock('/mock/A', 'get', {
+    'counter|0-9': 1,
+    'name': 'leo',
+    'desc': {
+        'age': 15,
+        'sex': 'male',
+        'favorite': 'female',
+    }
+})
+
+
+Mock.mock('/mock/B', 'post', function(opts) {
+    console.log('opts', opts);
+    return {
+        'desc': {
+            'age': 15,
+            'sex': 'male',
+            'favorite': 'female',
+        }
+    }
+})
+
+
+
+Mock.mock('/mock/C', 'get', {
+    name: {
+        first: '@FIRST',
+        middle: '@FIRST',
+        last: '@LAST',
+        full: '@first @middle @last'
+    }
+})
+
+Mock.mock('/mock/D', 'get', Random.image('200x100', '#4A7BF7', 'Hello'))
--- src/A/A.js
+++ src/A/A.js
@@ -1,8 +1,51 @@
 // const a = [1,2,3].map(d => d + 1);
 // console.log(a);
 import $ from 'jquery';
+// import 
+import '../../mock';
 
-$('.ttt').on('click', function() {
-    alert('fff');
-});
+// $.ajax({
+//     method: 'GET',
+//     url: '/mock/test',
+//     success: function(res) {
+//         console.log('res', res);
+//     }
+// });
 
+
+// $.ajax({
+//     method: 'GET',
+//     url: '/mock/A',
+//     success: function(res) {
+//         console.log('res', res);
+//     }
+// })
+
+// const params = { 'name': 'leo' };
+// $.ajax({
+//     method: 'POST',
+//     url: '/mock/B',
+//     data: JSON.stringify(params),
+//     contentType:"application/json",      
+//     dataType:"json",
+//     success: function(res) {
+//         console.log('res', res);
+//     }
+// })
+
+// $.ajax({
+//     method: 'GET',
+//     url: '/mock/C',
+//     success: function(res) {
+//         console.log('res', res);
+//     }
+// })
+
+$.ajax({
+    method: 'GET',
+    url: '/mock/D',
+    success: function(res) {
+        console.log('res', res); // url
+        $('body').append('<img src='+ res +' />');
+    }
+})
